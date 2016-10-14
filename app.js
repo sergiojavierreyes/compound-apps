dc11c86a0205930c7096bdb376fa95d84f218ed6
Sergio Reyes <sergio.javier.reyes@me.com>
2016-10-14 11:09:14 +0200

    loaded customer file

--- app.js
+++ app.js
@@ -3,5 +3,7 @@ const fs = require ('fs')
 
 // Read the customer data json
 fs.readFile(__dirname + "/customer.json", "utf8", (err,data)=>{
-
+	//parse the file to a readable object
+	let parsedData = JSON.parse(data)
+	console.log(parsedData)
 })
